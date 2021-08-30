e229ffd73999cf16a30cd91154fbfe30d24fc287
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-08-30 22:41:30 +0800

    Edited to conform the file names

--- waybar/waybar.sh
+++ waybar/waybar.sh
@@ -2,4 +2,4 @@
 
 killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
-waybar --config=$HOME/.config/waybar/riverconfig --style=$HOME/.config/waybar/riverstyle.css
+waybar --config=$HOME/.config/waybar/config --style=$HOME/.config/waybar/style.css
