fadeff976a6354bd94ec06d04f05e535830cf88f
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-08-31 15:06:13 +0800

    Finally fixed it lol

--- waybar/tag-states.sh
+++ waybar/tag-states.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
+
 RISTATE_LOG="$HOME/ristate.log"
-
-ristate -vt -t -w > $RISTATE_LOG &
-
-# Create a while loop that checks the status of the log
-# If log reaches 300 lines, then restart ristate
 while true
 do
-    RISTATE_STATE=$(pgrep ristate)
-    RISTATE_LOG_LINES=$(wc -l $RISTATE_LOG | awk '{ print $1 }')
     sleep 10s
-    if [[ ! $RISTATE_STATE -eq null ]]; then
+    if [[ $(pgrep -x ristate) -eq null ]]; then
         ristate -vt -t -w > $RISTAGE_LOG &
+		continue
     else
+    	RISTATE_LOG_LINES=$(wc -l $RISTATE_LOG | awk '{ print $1 }')
         if [[ $RISTATE_LOG_LINES -ge 10 ]]; then
             pkill ristate
+			continue
         else
             continue
         fi
