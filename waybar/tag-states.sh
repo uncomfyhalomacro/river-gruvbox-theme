aec087d910d82d165f2907dbc7319c076598ce40
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-07 10:56:21 +0800

    Cleaning up some scripts

--- waybar/tag-states.sh
+++ waybar/tag-states.sh
@@ -4,12 +4,8 @@
 export RISTATE_LOG="$HOME/ristate.log"
 while true
 do
-    #if [[ $(pgrep -x ristate) -eq null ]]; then
     ristate -vt -t -w > $RISTATE_LOG &
     sleep 30s
     killall -q ristate
-    #else
-        #RISTATE_LOG_LINES=$(wc -l $RISTATE_LOG | awk '{ print $1 }')
-        #[[ $RISTATE_LOG_LINES -ge 1000 ]] && killall -q ristate
 
 done
