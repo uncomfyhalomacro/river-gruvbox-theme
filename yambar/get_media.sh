9da758316e71851f11f6ce39254459de416b77a8
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-10 18:31:52 +0800

    Improve spotify custom script

--- yambar/get_media.sh
+++ yambar/get_media.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-media=$(playerctl -p spotify metadata -f "{{artist}} - {{title}}")
-player_status=$(playerctl -p spotify status)
+while true; do
+	media=$(playerctl -p spotify metadata -f "{{artist}} - {{title}}")
+	player_status=$(playerctl -p spotify status)
 
-if [[ $player_status = "Playing" ]]
-then
-    song_status=' '
-	echo -e "status|string|$song_status $media"
-elif [[ $player_status = "Paused" ]]
-then
-    song_status=' '
-	echo -e "status|string|$song_status $media"
-else
-    song_status=' spotify'
-	echo -e "status|string|$song_status"
-fi
-
-echo ""
+	if [[ $player_status = "Playing" ]]
+	then
+		song_status=' '
+		echo -e "status|string|$song_status $media"
+	elif [[ $player_status = "Paused" ]]
+	then
+		song_status=' '
+		echo -e "status|string|$song_status $media"
+	else
+		song_status=' spotify'
+		echo -e "status|string|$song_status"
+	fi
+	sleep 1
+	echo ""
+done
