dc68aca09214f734ff5e6cd261e19d99f2ff3f20
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-10 22:59:43 +0800

    Become catsu

--- river/swaybg.sh
+++ river/swaybg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ -f /usr/bin/swaybg ]]; then
-	swaybg -m fill -i $HOME/.config/river/background.png
+	swaybg -m fill -i $HOME/Pictures/zeke-tucker-gSSK4u8yPpM-unsplash.jpg
 fi
