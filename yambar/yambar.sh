2d9b5bd9bac1654b44754459c16ad2784e9f8db9
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-09-11 16:01:22 +0800

    Some updates

--- yambar/yambar.sh
+++ yambar/yambar.sh
@@ -2,5 +2,5 @@
 
 killall -q yambar
 while pgrep -x yambar >/dev/null; do sleep 1; done
-exec yambar --config $HOME/.config/yambar/bottom.yml & disown
-exec yambar --config $HOME/.config/yambar/top.yml & disown
+#exec yambar --config $HOME/.config/yambar/bottom.yml & disown
+exec yambar
